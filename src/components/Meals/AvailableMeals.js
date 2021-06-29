6b46db608ed69d6e37e3e025f9355caf6594232d
Patrykshub <63500126+Patrykshub@users.noreply.github.com>
2021-06-29 14:12:49 +0100

    feat: Loader

--- src/components/Meals/AvailableMeals.js
+++ src/components/Meals/AvailableMeals.js
@@ -1,35 +1,51 @@
 import { useEffect, useState } from "react";
 
 import classes from "./AvailableMeals.module.css";
-
+import './Loader.css';
 import MealItem from "./MealItem/MealItem";
 import Card from "../UI/Card";
 
 const AvailableMeals = () => {
-  const [meals, setMeals]= useState([]);
+  const [meals, setMeals] = useState([]);
+   const [isLoading, setIsLoading] = useState(false);
+
+   
+
   useEffect(() => {
     const fetchMeals = async () => {
+      setIsLoading(true);
       const response = await fetch(
         "https://react-my-burger360-default-rtdb.firebaseio.com/meals.json"
       );
       const responseData = await response.json();
-
+      
       const loadedMeals = [];
-
+        setIsLoading(false);
       for (const key in responseData) {
         loadedMeals.push({
           id: key,
           name: responseData[key].name,
           description: responseData[key].description,
           price: responseData[key].price,
-
-
-        })
+        });
       }
       setMeals(loadedMeals);
     };
     fetchMeals();
   }, []);
+
+  let content = <p>No meals yet.</p>
+
+  if (isLoading) {
+    content = (
+      <div className="lds_ring">
+        <div></div>
+        <div></div>
+        <div></div>
+        <div></div>
+      </div>
+    );
+  }
 
   const mealsList = meals.map((meal) => (
     <MealItem
@@ -43,7 +59,8 @@ const AvailableMeals = () => {
   return (
     <section className={classes.meals}>
       <Card>
-        <ul>{mealsList}</ul>
+      
+        <ul>{ isLoading ? content : mealsList}</ul>
       </Card>
     </section>
   );
